8ef7a4e67aad8300faa5d45e38c48627c475cabb
yarahmadi0077 <yarahmadi0077@gmail.com>
2024-05-12 23:50:52 +0330

    "4st commit of M.Yarahmadi of Main class about note's menu."

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,52 @@ public class Main {
     private static NotePad notePad = new NotePad();
     private static Scanner scanner = new Scanner(System.in);
 
+    public static void main(String[] args) throws IOException {
+        boolean running = true;
+        while (running) {
+            System.out.println("-------------------");
+            System.out.println("1.add new note");
+            System.out.println("2.remove note");
+            System.out.println("3.show all notes");
+            System.out.println("4.export");
+            System.out.println("5.exit");
+            System.out.println("-------------------");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 1:
+                    String temp = addNoteMenu() ;
+                    if(temp.equals("5"))
+                        break;
+                    if(temp.equals("note-added") || temp.equals("duplicate-title"))
+                        System.out.println(temp);
+                    break;
+                case 2:
+                    String temp1 = removeNoteMenu() ;
+                    if(temp1.equals("5"))
+                        break;
+                    if(temp1.equals("note-removed") || temp1.equals("notfound-note"))
+                        System.out.println(temp1);
+                    break;
+                case 3:
+                    notePad.listNotes();
+                    break;
+                case 4:
+                    if(exportNoteMenu().equals("5"))
+                        break;
+                    exportNoteMenu();
+                    break;
+                case 5:
+                    running = false;
+                    break;
+                default:
+                    System.out.println("invalid");
+            }
+        }
+        scanner.close();
+    }
 
     private static String addNoteMenu() {
         System.out.println("to back to menu enter 5 else enter the title");
