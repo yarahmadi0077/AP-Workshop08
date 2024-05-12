ee474e34aef13fe18df2141a798b8c3e83462400
yarahmadi0077 <yarahmadi0077@gmail.com>
2024-05-12 23:45:32 +0330

    "1st commit of M.Yarahmadi of Main class about note's menu."

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
-public class Main {
+import java.io.IOException;
+import java.util.Scanner;
 
+public class Main {
+    private static NotePad notePad = new NotePad();
+    private static Scanner scanner = new Scanner(System.in);
 }
