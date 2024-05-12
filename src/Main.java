ef475009561f94ef6c09d979391fd0b6773d82df
yarahmadi0077 <yarahmadi0077@gmail.com>
2024-05-12 23:49:34 +0330

    "1st commit of M.Yarahmadi of Main class about note's menu."

--- src/Main.java
+++ src/Main.java
@@ -4,4 +4,32 @@ import java.util.Scanner;
 public class Main {
     private static NotePad notePad = new NotePad();
     private static Scanner scanner = new Scanner(System.in);
+
+
+    private static String addNoteMenu() {
+        System.out.println("to back to menu enter 5 else enter the title");
+        String title = scanner.nextLine();
+        if(title.equals("5"))
+            return "5";
+        System.out.println("enter the text");
+        String content = scanner.nextLine();
+        return (notePad.addNote(title, content));
+    }
+
+    private static String removeNoteMenu() {
+        System.out.println("to back to menu enter 5 else enter the title");
+        String title = scanner.nextLine();
+        if(title.equals("5"))
+            return "5";
+        return (notePad.removeNote(title));
+    }
+
+    private static String exportNoteMenu() throws IOException {
+        System.out.println("to back to menu enter 5 else enter the title");
+        String title = scanner.nextLine();
+        if(title.equals("5"))
+            return "5";
+        notePad.exportNote(title);
+        return "";
+    }
 }
